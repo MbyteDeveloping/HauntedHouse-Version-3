7956cf8b831c6860f34045addc871f18a808ec4e
Jacobferdinand <hello@jacobfs.tech>
2023-04-05 13:59:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
     })
     gsap.to('#exit-wrapper', { display: 'block', zIndex: 30 })
     handleOrbitButtonClick()
-    document.body.style.overflow = 'hidden'
+
   }
 
   function unOrbitButtonClick() {
@@ -56,7 +56,7 @@ export default function App() {
     })
     gsap.to('#exit-wrapper', { display: 'none', zIndex: 0 })
     handleUnOrbitButtonClick()
-    document.body.style.overflow = 'auto'
+
   }
 
   let orbitButton = document.getElementById('orbit-button')
