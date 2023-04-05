29f20d1c12f4cfdf565b8fd9fc9a3eaf104652c2
Jacobferdinand <hello@jacobfs.tech>
2023-04-05 20:04:50 +0200

    Update animations.jsx

--- src/animations.jsx
+++ src/animations.jsx
@@ -2,162 +2,157 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
-import { useEffect } from 'react'
+export default function animations() {
+  var Cont = { val: 0 },
+    NewVal = 100
 
-export default function animations(props) {
-  useEffect(() => {
-    var Cont = { val: 0 },
-      NewVal = 100
+  let tl = gsap.timeline({
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.section-wrapper',
+      start: 'top top',
+      end: 'bottom bottom',
+      pin: false,
+      scrub: 1,
+    },
+  })
 
-    let tl = gsap.timeline({
-      ease: 'none',
-      scrollTrigger: {
-        trigger: '.section-wrapper',
-        start: 'top top',
-        end: 'bottom bottom',
-        pin: false,
-        scrub: 1,
-
-      },
-    })
-
-    tl.to(
-      Cont,
-      {
-        duration: 7,
-        ease: 'none',
-        val: NewVal,
-        roundProps: 'val',
-        onUpdate: function () {
-          document.querySelector('.progress-number').innerHTML = Cont.val + '%'
-        },
-      },
-      0
-    )
-
-    gsap.to('.progress-bar', {
-      scrollTrigger: {
-        trigger: '.section-wrapper',
-        start: 'top top',
-        end: 'bottom bottom',
-        scrub: 1,
-      },
-      width: '100%',
+  tl.to(
+    Cont,
+    {
       duration: 7,
       ease: 'none',
-    })
-
-    gsap.to('#content-1', {
-      scrollTrigger: {
-        trigger: '#section-1',
-        start: '100% 80%',
-        endtrigger: '#section-1',
-        end: '0% 50%',
-        toggleActions: 'restart restart reverse reverse',
+      val: NewVal,
+      roundProps: 'val',
+      onUpdate: function () {
+        document.querySelector('.progress-number').innerHTML = Cont.val + '%'
       },
+    },
+    0
+  )
+
+  gsap.to('.progress-bar', {
+    scrollTrigger: {
+      trigger: '.section-wrapper',
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: 1,
+    },
+    width: '100%',
+    duration: 7,
+    ease: 'none',
+  })
+
+  gsap.to('#content-1', {
+    scrollTrigger: {
+      trigger: '#section-1',
+      start: '100% 80%',
+      endtrigger: '#section-1',
+      end: '0% 50%',
+      toggleActions: 'restart restart reverse reverse',
+    },
+    opacity: 0,
+    zIndex: 0,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
+
+  gsap.to('#content-2', {
+    scrollTrigger: {
+      trigger: '#section-2',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart reverse restart reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
+  let navDissappear = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#section-2',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart none none reverse',
+    },
+  })
+  navDissappear
+    .to('.nav-link', {
       opacity: 0,
-      zIndex: 0,
-      duration: 0.8,
+      duration: 0.7,
       ease: 'Power2.Out',
+    })
+    .to('#nav-button-text', {
+      opacity: 0,
+      width: 0,
+      display: 'none',
+      duration: 0.4,
+      ease: 'Power1.Out',
     })
 
-    gsap.to('#content-2', {
-      scrollTrigger: {
-        trigger: '#section-2',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart reverse restart reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
-    let navDissappear = gsap.timeline({
-      scrollTrigger: {
-        trigger: '#section-2',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart none none reverse',
-      },
-    })
-    navDissappear
-      .to('.nav-link', {
-        opacity: 0,
-        duration: 0.7,
-        ease: 'Power2.Out',
-      })
-      .to('#nav-button-text', {
-        opacity: 0,
-        width: 0,
-        display: 'none',
-        duration: 0.4,
-        ease: 'Power1.Out',
-      })
+  gsap.to('#content-3', {
+    scrollTrigger: {
+      trigger: '#section-3',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart reverse restart reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
 
-    gsap.to('#content-3', {
-      scrollTrigger: {
-        trigger: '#section-3',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart reverse restart reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
+  gsap.to('#content-4', {
+    scrollTrigger: {
+      trigger: '#section-4',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart reverse restart reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
 
-    gsap.to('#content-4', {
-      scrollTrigger: {
-        trigger: '#section-4',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart reverse restart reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
+  gsap.to('#content-5', {
+    scrollTrigger: {
+      trigger: '#section-5',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart reverse restart reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
 
-    gsap.to('#content-5', {
-      scrollTrigger: {
-        trigger: '#section-5',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart reverse restart reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
+  gsap.to('#content-6', {
+    scrollTrigger: {
+      trigger: '#section-6',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart reverse restart reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
+  })
 
-    gsap.to('#content-6', {
-      scrollTrigger: {
-        trigger: '#section-6',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart reverse restart reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
-
-    gsap.to('#content-7', {
-      scrollTrigger: {
-        trigger: '#section-7',
-        start: '0% 20%',
-        end: '100% 100%',
-        toggleActions: 'restart none reverse reverse',
-      },
-      opacity: 1,
-      zIndex: 10,
-      duration: 0.8,
-      ease: 'Power2.Out',
-    })
+  gsap.to('#content-7', {
+    scrollTrigger: {
+      trigger: '#section-7',
+      start: '0% 20%',
+      end: '100% 100%',
+      toggleActions: 'restart none reverse reverse',
+    },
+    opacity: 1,
+    zIndex: 10,
+    duration: 0.8,
+    ease: 'Power2.Out',
   })
 }
